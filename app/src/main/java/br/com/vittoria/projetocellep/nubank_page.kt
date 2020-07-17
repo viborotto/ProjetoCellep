8ed97f1dc17c2c908ed0822ac7f7c5f58311acc4
viborotto <vittoriaborotto@gmail.com>
2020-07-17 20:19:43 -0300

    Layout Facebook

---
+++ app/src/main/java/br/com/vittoria/projetocellep/nubank_page.kt
@@ -0,0 +1,11 @@
+package br.com.vittoria.projetocellep
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class nubank_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nubank_page)
+    }
+}
